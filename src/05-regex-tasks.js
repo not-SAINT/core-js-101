a9533d216e35dbe398fb2cf316418706212363f0
Aleksandr Sharikov <notsaint.mail@gmail.com>
2020-05-11 22:14:54 +0300

    feat: add implementation regex-tasks

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /{[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}}/i;
 }
 
 
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /p[^tea]|sp/;
 }
 
 
@@ -78,10 +78,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return new RegExp(`(^\\S*)(?=.*[0-9])(?=.*[A-Za-z0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{${minLength},}`);
 }
-
 
 module.exports = {
   getRegexForGuid,
